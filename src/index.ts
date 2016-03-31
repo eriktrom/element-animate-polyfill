b4b6d59a57d75ab9c970cb67242e1eb70deb491b
Martin Staffa <mjstaffa@googlemail.com>
2016-03-31 16:28:30 +0200

    feat: basic animation for one pixel based property

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
+import {Animation} from "./animation";
+import {Player} from "./player";
+
 export class ElementAnimatePolyfill {
-  animate() {
-    return 'true';
+  animate(element: HTMLElement, keyframes: {[key: string]: string}[], options: number): Player {
+    return new Animation(keyframes, options).start(element);
   }
 }
 
 if (!Element.prototype['animate']) {
   var polyfill = new ElementAnimatePolyfill();
 
-  Element.prototype['animate'] = function() {
-    return polyfill.animate();
-  }
+  Element.prototype['animate'] = (keyframes, options) => polyfill.animate(this, keyframes, options);
 }
--- test/echo_spec.ts
+++ test/echo_spec.ts
@@ -3,6 +3,5 @@ import {ElementAnimatePolyfill} from '../src/index';
 describe('ElementAnimatePolyfill', () => {
   it('should be true', () => {
     var inst = new ElementAnimatePolyfill();
-    expect(inst.animate()).toEqual('true');
   });
 });
